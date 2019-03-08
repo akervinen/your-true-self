3055c06211dceda076eaa1559c761dd032a0ad1d
Aleksi Kervinen <me@aleksi.me>
2019-03-08 14:18:43 +0200

    use AssetManager again (now properly) for possible async loading

--- core/src/main/java/fi/tamk/yourtrueself/CharacterSelectScreen.java
+++ core/src/main/java/fi/tamk/yourtrueself/CharacterSelectScreen.java
@@ -2,7 +2,6 @@ package fi.tamk.yourtrueself;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
--- core/src/main/java/fi/tamk/yourtrueself/YTSGame.java
+++ core/src/main/java/fi/tamk/yourtrueself/YTSGame.java
@@ -2,17 +2,14 @@ package fi.tamk.yourtrueself;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.assets.AssetDescriptor;
 import com.badlogic.gdx.assets.AssetManager;
-import com.badlogic.gdx.assets.loaders.FileHandleResolver;
-import com.badlogic.gdx.assets.loaders.resolvers.InternalFileHandleResolver;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.assets.loaders.SkinLoader;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
-import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGeneratorLoader;
-import com.badlogic.gdx.graphics.g2d.freetype.FreetypeFontLoader;
-import com.badlogic.gdx.graphics.g2d.freetype.FreetypeFontLoader.FreeTypeFontLoaderParameter;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.utils.ObjectMap;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
@@ -21,6 +18,11 @@ import com.badlogic.gdx.utils.viewport.Viewport;
  */
 public class YTSGame extends Game {
     public static final String SKIN_PATH = "ui/orange/skin.json";
+
+    private static final AssetDescriptor<TextureAtlas> characterAtlasAsset =
+            new AssetDescriptor<TextureAtlas>("characters.atlas", TextureAtlas.class);
+
+    private AssetManager assetManager = new AssetManager();
 
     private Viewport uiViewport;
     private Skin uiSkin;
@@ -35,26 +37,30 @@ public class YTSGame extends Game {
     }
 
     private void loadSkin() {
-        uiSkin = new Skin();
-
         FreeTypeFontGenerator.FreeTypeFontParameter parameter;
         FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("ui/Roboto-Regular.ttf"));
         parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
         parameter.size = getPointInPixels(10);
         BitmapFont fontSmall = generator.generateFont(parameter);
 
-
         parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
         parameter.size = getPointInPixels(18);
         BitmapFont fontTitle = generator.generateFont(parameter);
         generator.dispose();
 
-        uiSkin.add("font", fontSmall, BitmapFont.class);
-        uiSkin.add("font-title", fontTitle, BitmapFont.class);
+        ObjectMap<String, Object> fontMap = new ObjectMap<String, Object>();
+        fontMap.put("font", fontSmall);
+        fontMap.put("font-title", fontTitle);
 
-        uiSkin.addRegions(new TextureAtlas("ui/orange/skin.atlas"));
-        uiSkin.addRegions(new TextureAtlas("characters.atlas"));
-        uiSkin.load(Gdx.files.internal(SKIN_PATH));
+        SkinLoader.SkinParameter skinParam = new SkinLoader.SkinParameter(fontMap);
+
+        assetManager.load(characterAtlasAsset);
+        assetManager.load(SKIN_PATH, Skin.class, skinParam);
+
+        assetManager.finishLoading();
+
+        uiSkin = assetManager.get(SKIN_PATH);
+        uiSkin.addRegions(assetManager.get(characterAtlasAsset));
     }
 
     Viewport getUiViewport() {
@@ -96,6 +102,6 @@ public class YTSGame extends Game {
 
     @Override
     public void dispose() {
-        uiSkin.dispose();
+        assetManager.dispose();
     }
 }
