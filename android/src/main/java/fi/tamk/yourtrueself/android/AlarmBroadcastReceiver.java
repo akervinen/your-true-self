b7b33580addc00a4351b8593a04f83e0c0a8e822
Aleksi Kervinen <me@aleksi.me>
2019-03-25 13:38:20 +0200

    implement notification clicking to open the game

--- android/src/main/java/fi/tamk/yourtrueself/android/AlarmBroadcastReceiver.java
+++ android/src/main/java/fi/tamk/yourtrueself/android/AlarmBroadcastReceiver.java
@@ -2,6 +2,7 @@ package fi.tamk.yourtrueself.android;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -13,11 +14,14 @@ import fi.tamk.yourtrueself.R;
 public class AlarmBroadcastReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
+        PendingIntent contentIntent = PendingIntent.getActivity(context, 0, new Intent(context, AndroidLauncher.class), PendingIntent.FLAG_UPDATE_CURRENT);
+
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "your-true-self")
                 .setSmallIcon(R.drawable.ic_launcher)
                 .setContentTitle("New Challenge!")
                 .setContentText("You got a new challenge in Your True Self!")
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(contentIntent);
 
         NotificationManager am = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 
