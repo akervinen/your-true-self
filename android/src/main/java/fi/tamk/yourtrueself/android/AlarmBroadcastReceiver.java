f178422b2a79de5350c8370b69ee4338ef309d14
Aleksi Kervinen <me@aleksi.me>
2019-03-25 15:00:19 +0200

    add i18n support for notifications and change according to prefs

--- android/src/main/java/fi/tamk/yourtrueself/android/AlarmBroadcastReceiver.java
+++ android/src/main/java/fi/tamk/yourtrueself/android/AlarmBroadcastReceiver.java
@@ -9,17 +9,38 @@ import android.content.Intent;
 import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Preferences;
+import com.badlogic.gdx.backends.android.AndroidFiles;
+import com.badlogic.gdx.backends.android.AndroidPreferences;
+import com.badlogic.gdx.utils.I18NBundle;
+
+import java.util.Locale;
+
 import fi.tamk.yourtrueself.R;
+import fi.tamk.yourtrueself.YTSGame;
 
 public class AlarmBroadcastReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         PendingIntent contentIntent = PendingIntent.getActivity(context, 0, new Intent(context, AndroidLauncher.class), PendingIntent.FLAG_UPDATE_CURRENT);
 
+        Preferences pref = new AndroidPreferences(context.getSharedPreferences(YTSGame.PREF_NAME, Context.MODE_PRIVATE));
+        Locale locale;
+        if (pref.contains("lang")) {
+            String lang = pref.getString("lang");
+            locale = YTSGame.localeFromPref(lang);
+        } else {
+            locale = Locale.getDefault();
+        }
+
+        Gdx.files = new AndroidFiles(context.getAssets(), context.getFilesDir().getAbsolutePath());
+        I18NBundle bundle = I18NBundle.createBundle(Gdx.files.internal("i18n/YourTrueSelf"), locale);
+
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "your-true-self")
                 .setSmallIcon(R.drawable.ic_launcher)
-                .setContentTitle("New Challenge!")
-                .setContentText("You got a new challenge in Your True Self!")
+                .setContentTitle(bundle.get("chlNotificationTitle"))
+                .setContentText(bundle.get("chlNotificationBody"))
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT)
                 .setContentIntent(contentIntent);
 
