d6730b8317c670d8ca2ab6dc2ff1f79557f3ef6f
Aleksi Kervinen <me@aleksi.me>
2019-04-11 19:44:41 +0300

    update palettes and UI graphics

--- raw/aseprite-gen.bat
+++ raw/aseprite-gen.bat
@@ -1,5 +1,10 @@
 @echo off
 
+REM basic color drawable
+
+aseprite -b ui-ase/color.9.aseprite --palette palette-primary.pal --save-as ui/primary.9.png
+aseprite -b ui-ase/color.9.aseprite --palette palette-secondary.pal --save-as ui/secondary.9.png
+
 REM panel
 
 aseprite -b ui-ase/panel.9.aseprite --palette palette-primary.pal --save-as ui/panel-primary.9.png
@@ -20,3 +25,10 @@ aseprite -b ui-ase/button.9.aseprite --layer down --palette palette-primary.pal 
 
 aseprite -b ui-ase/button.9.aseprite --layer up --palette palette-secondary.pal --save-as ui/button-secondary.9.png
 aseprite -b ui-ase/button.9.aseprite --layer down --palette palette-secondary.pal --save-as ui/button-secondary-down.9.png
+
+aseprite -b ui-ase/button.9.aseprite --layer up --palette palette-good.pal --save-as ui/button-good.9.png
+aseprite -b ui-ase/button.9.aseprite --layer down --palette palette-good.pal --save-as ui/button-good-down.9.png
+
+REM progress bar
+
+aseprite -b ui-ase/progress-bar.9.aseprite --palette palette-primary.pal --save-as ui/progress-bar.9.png
